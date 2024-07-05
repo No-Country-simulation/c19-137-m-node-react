278bb9a405552f6e1556aef4ad97ab8746a7f562
Sensitiky <mariomcorrea3@gmail.com>
2024-07-05 12:29:02 -0300

    :seedling: Second commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Accordion } from "@/components/ui/accordion";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -38,7 +40,6 @@ export default function Home() {
           priority
         />
       </div>
-
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"

b41946bd182c2e3e6809b95efd0d67e932d7d92e
YEIMAR LEMUS <yeimar112003@outlook.com>
2024-07-11 09:22:35 -0500

    :sparkles:  feat: se agrega la estructura basica para la api de graphQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Configuración de TypeORM
+    TypeOrmModule.forRoot({
+      type: 'postgres', // o el tipo de base de datos que uses (mysql, sqlite, etc.)
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'mydatabase',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true, // No usar en producción
+    }),
+    // Configuración de GraphQL
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+      playground: true,  // Desactivar en producción
+    }),
+    // Módulos de la aplicación
+    UsersModule,
+    CommentsModule,
+    MediaModule,
+    ProfilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}

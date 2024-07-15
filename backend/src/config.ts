75fb1b7d595455f6a1205a3ea08bbd499f8d3cea
YEIMAR LEMUS <yeimar112003@outlook.com>
2024-07-15 08:27:33 -0500

    feat: se actualizan dependencias de la api y se configura el app module

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,17 +1,12 @@
-import {registerAs} from '@nestjs/config';
+import { registerAs } from '@nestjs/config';
 
 export default registerAs('config', () => ({
-    database: {
-        name: process.env.DATABASE_NAME,
-        port: process.env.PORT
-    },
-    postgres: {
-        db_name: process.env.POSTGRES_DB,
-        port: parseInt(process.env.POSTGRES_PORT, 10),
-        password: process.env.POSTGRES_PASSWORD,
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST
-    },
-    apiKey: process.env.API_KEY
+  db: {
+    name: process.env.DATABASE_NAME,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    password: process.env.DATABASE_PASSWORD,
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+  },
 }));
 

a0e289c073e7780804662bff48a56d0f6d0ed4f7
Juan Figuera <juanantoniofiguera@gmail.com>
2024-07-18 18:07:05 +0200

    feat: :sparkles: creado el modulo posts con todos sus archivos

---
+++ backend/src/posts/posts.resolver.ts
@@ -0,0 +1,29 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { PostsService } from './posts.service';
+import { Post } from './entities/post.entity';
+import { GqlAuthGuard } from 'src/modules/auth/guards/gql-auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { CreatePostInput } from './dto/create-post-input';
+
+@Resolver(() => Post)
+export class PostsResolver {
+  constructor(private readonly postsService: PostsService) {
+  }
+  @Query('posts')
+  findAll() {
+    return this.postsService.findAll();
+  }
+
+  @Query('post')
+  findById(@Args('id') id: string ){
+    return this.postsService.findById(id);
+  }
+
+  @Mutation('createPost')
+  @UseGuards(GqlAuthGuard)
+  createPost(@Args('data') data: CreatePostInput) {
+    return this.postsService.createPost(data)
+  }
+
+
+}

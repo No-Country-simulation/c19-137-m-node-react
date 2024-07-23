c05df03c391416e1e30cfcb20d21c3922cfc3b0f
Juan Figuera <juanantoniofiguera@gmail.com>
2024-07-23 11:21:43 +0200

    feat: :sparkles: agregada relacion de usuario a posts y usuario a libros

--- backend/src/modules/users/entities/user.entity.ts
+++ backend/src/modules/users/entities/user.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Post } from 'src/modules/posts/entities/post.entity';
+import { Book } from 'src/modules/books/entities/book.entity';
 
 @Entity()
 export class User {
@@ -7,6 +9,13 @@ export class User {
 
   @Column({ unique: true })
   nickname: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
+
+  @ManyToMany(() => Book, (book) => book.users)
+  @JoinTable()
+  favorites: Book[];
 
   @Column()
   first_name: string;
--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { PasswordResetToken } from './entities/password-reset-token.entity';
 import { UsersResolver } from './users.resolver';
+import { Book } from '../books/entities/book.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, PasswordResetToken])],
+  imports: [TypeOrmModule.forFeature([User, PasswordResetToken, Book])],
   providers: [UsersService, UsersResolver],
   exports: [UsersService],
 })

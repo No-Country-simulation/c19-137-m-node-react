abbd9b7832ce0cf63f0ce412ceba1702240cf578
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-07-24 12:57:38 -0300

    update 0.2

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,62 @@
-import NextAuth, {AuthOptions} from "next-auth";
-import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
+import NextAuth, { AuthOptions } from "next-auth";
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import CredentialsProvider from "next-auth/providers/credentials";
-import type { NextAuthOptions } from "next-auth"
+import { NextAuthOptions } from "next-auth"
 
 const client = new ApolloClient({
     uri: process.env.GRAPHQL_ENDPOINT,
     cache: new InMemoryCache(),
 });
 
-
-
 const secret = `${process.env.JWT_SECRET}`
 
-
-// 
-
 const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "email", type: "email", placeholder: "test@test.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`,
-          {
-            method: "POST",
-            body: JSON.stringify({
-              email: credentials?.email,
-              password: credentials?.password,
-            }),
-            headers: { 
-              "Content-Type": "application/json" 
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "email", type: "email", placeholder: "test@test.com" },
+                password: { label: "Password", type: "password" },
             },
-          }
-        );
-        const user = await res.json();
+            async authorize(credentials) {
+                try {
+                    const { data } = await client.mutate({
+                        mutation: gql`
+              mutation Login($email: String!, $password: String!) {
+                login(email: $email, password: $password) {
+                  id
+                  name
+                  email
+                  token
+                }
+              }
+            `,
+                        variables: {
+                            email: credentials?.email,
+                            password: credentials?.password,
+                        },
+                    });
 
+                    const user = data.login;
 
-        return user;
-      },
-    }),
-  ],
+                    if (user) {
+                        console.log(user)
+                        return {
+                            id: user.id,
+                            name: user.name,
+                            email: user.email,
+                            token: user.token,
+                        };
+                    } else {
+                        return null;
+                    }
+                } catch (error) {
+                    console.error("Error in authorize:", error);
+                    return null;
+                }
+            },
+        }),
+    ],
 
 });
 

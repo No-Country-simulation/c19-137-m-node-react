928eb7ad1513c0894d1fc2c52cf4af693761e044
Juan Figuera <juanantoniofiguera@gmail.com>
2024-07-25 11:50:16 +0200

    chore: :pencil2: crear libro favorito ahora usa el nuevo decorador

--- backend/src/modules/users/dto/add-favorite-book.input.ts
+++ backend/src/modules/users/dto/add-favorite-book.input.ts
@@ -4,13 +4,7 @@ import { IsNotEmpty, IsUUID } from 'class-validator';
 @InputType()
 export class addFavoriteBookInput {
   @Field()
-  @IsNotEmpty({ message: 'El usuario es requerido' })
-  @IsUUID()
-  userId: string;
-
-  @Field()
   @IsNotEmpty({ message: 'El libro es requerido' })
   @IsUUID()
   bookId: string;
-
 }
